1b51862b3049b2e7d8cef45ad93e15a1b69bda6b
ChiefArug <73862885+ChiefArug@users.noreply.github.com>
2023-09-20 19:16:32 +1200

    Begin work on replacing some loot instead of just adding. No functional changes, just commented out code

--- src/main/java/chiefarug/mods/thermaloot/loot/AddACapacitorModifier.java
+++ src/main/java/chiefarug/mods/thermaloot/loot/AddACapacitorModifier.java
@@ -24,14 +24,17 @@ public class AddACapacitorModifier extends LootModifier {
 
     public static final Codec<AddACapacitorModifier> CODEC = RecordCodecBuilder.create(
             instance -> LootModifier.codecStart(instance).and(
-                    ICodecifiedNumberProvidersForYouMojangHopeYoureHappy.NUMBER_PROVIDER.optionalFieldOf("count", SINGLE_COUNT).forGetter(AddACapacitorModifier::getCountProvider) //TODO: use NullableFieldCodec from https://github.com/Commoble/databuddy/blob/b52374e6b8177ab1a360b74c3b31c7b268741473/src/main/java/commoble/databuddy/codec/NullableFieldCodec.java#L46 here so that it doesn't just SILENTLY fail
+                    ICodecifiedNumberProvidersForYouMojangHopeYoureHappy.NUMBER_PROVIDER.optionalFieldOf("count", SINGLE_COUNT).forGetter(AddACapacitorModifier::getCountProvider)//).and( //TODO: use NullableFieldCodec from https://github.com/Commoble/databuddy/blob/b52374e6b8177ab1a360b74c3b31c7b268741473/src/main/java/commoble/databuddy/codec/NullableFieldCodec.java#L46 here so that it doesn't just SILENTLY fail
+//                    Codec.FLOAT.fieldOf("replace_chance").forGetter(AddACapacitorModifier::getReplaceChance)
             ).apply(instance, AddACapacitorModifier::new)
     );
 
     private final NumberProvider countProvider;
-    protected AddACapacitorModifier(LootItemCondition[] conditionsIn, NumberProvider countProvider) {
+//    private final float replaceChance;
+    protected AddACapacitorModifier(LootItemCondition[] conditionsIn, NumberProvider countProvider/*, float replaceChance*/) {
         super(conditionsIn);
         this.countProvider = countProvider;
+//        this.replaceChance = replaceChance;
     }
 
     private boolean noRecurse = false;
@@ -41,9 +44,19 @@ public class AddACapacitorModifier extends LootModifier {
         if (noRecurse || context.getQueriedLootTableId().equals(SINGLE_CAPACITOR)) return generatedLoot;
          
         int repeats = countProvider.getInt(context);
+//        List<ItemStack> stackedStacks = generatedLoot.stream().collect(Collectors.groupingBy(ItemStack::getItem)).values().stream().map(itemStacks -> {
+//            ItemStack stack = itemStacks.get(0).copy();
+//            stack.setCount(itemStacks.size());
+//            return stack;
+//        }).sorted(Comparator.comparingInt(ItemStack::getCount)).toList();
+
         noRecurse = true;
         while (repeats-- >= 1)
             generatedLoot.addAll(context.getLevel().getServer().getLootTables().get(SINGLE_CAPACITOR).getRandomItems(context));
+//            if (context.getRandom().nextFloat() < replaceChance) {
+//                stackedStacks.
+//            } else
+//                generatedLoot.addAll(context.getLevel().getServer().getLootTables().get(SINGLE_CAPACITOR).getRandomItems(context));
         noRecurse = false;
 
         return generatedLoot;
@@ -52,6 +65,10 @@ public class AddACapacitorModifier extends LootModifier {
     public NumberProvider getCountProvider() {
         return countProvider;
     }
+
+//    public float getReplaceChance() {
+//        return replaceChance;
+//    }
 
     @Override
     public Codec<? extends IGlobalLootModifier> codec() {
